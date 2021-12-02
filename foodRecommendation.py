05603c9c8a528496dcfde1353e832eb924101950
JesseKartabani <jessekartabani@gmail.com>
2021-12-03 04:41:45 +1100

    tkinter boilerplate

--- foodRecommendation.py
+++ foodRecommendation.py
@@ -1,8 +1,22 @@
-from tkinter import *
+# A simple tkinter window
+import tkinter as tk
 from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+# -- Windows only configuration --
+try:
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+except:
+    pass
+# -- End Windows only configuration --
+
+
+root = tk.Tk()
+root.geometry("400x300")
+root.title("Food Recommendations")
+
+label = ttk.Label(root, padding=10, text="Enter your favourite meals")
+label.config(font=("Times New Roman",20))
+label.pack()
+
 root.mainloop()
